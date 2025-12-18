616eb1d119ac48b86e71e5517c8137a181a71021
Tobias Wagner <tobawa2601@gmail.com>
2025-12-18 20:05:09 +0100

    Created a sealed class for the player package, that holds all message objects a player can sent. Currently only the buzzing message.

---
+++ src/main/kotlin/de/tkay/quiz/player/message/PlayerIncomingMessage.kt
@@ -0,0 +1,17 @@
+package de.tkay.quiz.player.message
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonClassDiscriminator
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+@JsonClassDiscriminator("type")
+sealed class PlayerIncomingMessage {
+
+    @Serializable
+    @SerialName("BUZZING")
+    data object Buzzing : PlayerIncomingMessage()
+
+}
